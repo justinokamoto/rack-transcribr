0aeed7c5a1fdbbdd74c10e0d68dd3035a5455cc5
Justin Okamoto <justin@bellwethermade.com>
2016-03-28 22:53:24 -0500

    Added transcribr class

---
+++ lib/rack/transcribr.rb
@@ -0,0 +1,27 @@
+require "rack/extensions/string"
+require "transcribr/version"
+
+module Rack
+  class Transcribr
+    def initialize(app, args)
+      @app = app
+      @filepath = args["log_filepath"] || "./raw_html.log"
+    end 
+
+    def call(env)
+      status, headers, response = @app.call(env)
+      log_file = File.new(@filepath, "w")
+
+      headers.each do |line|
+        log_file.puts(line)
+      end
+      response.each do |line|
+        log_file.puts(line)
+      end
+
+      log_file.close
+
+      [status, headers, response]
+    end
+  end
+end
