6706d2cb0529c0fc17e076fda99ca8676d0db7da
Justin Okamoto <justin@bellwethermade.com>
2016-04-09 19:46:32 -0500

    Overriding CommonLogger and colorizing output

--- lib/rack/transcribr.rb
+++ lib/rack/transcribr.rb
@@ -1,57 +1,74 @@
+require 'rack/body_proxy'
 require "rack/transcribr/extensions"
 
 module Rack
+
+  class CommonLogger
+    def log(env, status, header, began_at) end # Disable CommonLogger middleware
+  end
+
   class Transcribr
-    def initialize(app)
+
+    FORMAT = %{%s - %s [%s] "%s %s%s %s" %d %s %0.4f\n}
+
+    def initialize(app, logger=nil)
       @app = app
-      # @filepath = args["log_filepath"] || "./raw_html.log"
-    end 
-
-    def call(env)
-      status, headers, response = @app.call(env)
-
-      # log_file = File.new(@filepath, "w")
-      body = ""
-      puts headers["Content-Type"]
-      puts "USER AGENT".red
-      puts headers
-      puts "ENV"
-      puts env
-      response.each { |line| puts line }
-      response.each { |line| body << line }
-      body << hello_world
-      headers["Content-Length"] = body.length.to_s
-      response = [body]
-      # if headers["Content-Type"] =~ /text\/html|application\/xhtml\+xml/
-      #   puts "html"
-      #   body = ""
-      #   response.each { |part| body << part; puts part }
-      #   index = body.rindex("</body>")
-
-
-      #   if index
-      #     body.insert(index, hello_world)
-      #     headers["Content-Length"] = body.length.to_s
-      #     response = [body]
-      #   end
-      # else
-      #   puts "not html"
-      #   body = ""
-      #   response.each { |part| body << part; puts }
-      # end
-
-      [status, headers, response]
+      @logger = logger
     end
 
-    def hello_world
-      returning_value = <<-EOF
-<script type="text/javascript">
-console.log("Hello, world.");
-</script>
-EOF
-      returning_value
+    def call(env)
+      began_at = Time.now
+      status, header, body = @app.call(env)
+      header = Utils::HeaderHash.new(header)
+      body = BodyProxy.new(body) { log(env, status, header, began_at) }
+      [status, header, body]
+    end
+
+    private
+
+    def log(env, status, header, began_at)
+      now = Time.now
+      length = extract_content_length(header)
+
+      msg = FORMAT % [
+        env['HTTP_X_FORWARDED_FOR'] || env["REMOTE_ADDR"] || "-",
+        env["REMOTE_USER"] || "-",
+        now.strftime("%d/%b/%Y:%H:%M:%S %z"),
+        env["REQUEST_METHOD"],
+        env["PATH_INFO"],
+        env["QUERY_STRING"].empty? ? "" : "?"+env["QUERY_STRING"],
+        env["HTTP_VERSION"],
+        status.to_s[0..3],
+        length,
+        now - began_at ]
+
+      logger = @logger || env["rack.errors"]
+
+      if logger.respond_to?(:write)
+        logger.write(colorize(env["REQUEST_METHOD"], msg))
+      else
+        logger << colorize(env["REQUEST_METHOD"], msg)
+      end
+    end
+
+    def extract_content_length(headers)
+      value = headers[CONTENT_LENGTH] or return '-'
+      value.to_s == '0' ? '-' : value
+    end
+
+    def colorize(method, msg)
+      case method
+      when "GET"
+        msg.blue
+      when "POST"
+        msg.green
+      when "PUT", "PATCH"
+        msg.yellow
+      when "DELETE"
+        msg.red
+      else
+        msg
+      end
     end
   end
 end
-
-# Logs header request information
